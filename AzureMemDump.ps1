9defc07fe3af920ec621b920635e54526d8a3350
jllangley <orig3n@gmail.com>
2020-04-20 10:12:09 -0500

    Add files via upload

---
+++ AzureMemDump.ps1
@@ -0,0 +1,3 @@
+﻿$memdumpurl = "https://github.com/Velocidex/c-aff4/releases/download/v3.3.rc3/winpmem_v3.3.rc3.exe"
+Invoke-WebRequest $memdumpurl -OutFile "C:\Programdata\winpmem.exe"
+c:\Programdata\winpmem.exe --output c:\ProgramData\memorydump.dmp
